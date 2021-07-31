66531fc417e77ccb1ab588ff0cf7bfa9492e85c7
SIDDARTH.D <siddarth.dayasagar@gmail.com>
2021-07-31 11:22:45 +0530

    Updated laser_reading

--- DAY23/laser_data.py
+++ DAY23/laser_data.py
@@ -14,6 +14,16 @@ def sensor_cb(msg):
     print("-----------------------------")
     print("RANGE at 90",msg.ranges[360-1])
     
+def laser_data(msg):
+    regions=[
+        min(min(msg.ranges[0:71]),10),
+        min(min(msg.ranges[72:143]),10),
+        min(min(msg.ranges[144:216]),10),
+        min(min(msg.ranges[217:288]),10),
+        min(min(msg.ranges[289:360]),10),
+    ]
+    rospy.loginfo(regions)
+    
 if __name__=="__main__":
     rospy.init_node('scan_data')
     rospy.Subscriber('/scan',LaserScan,sensor_cb)
